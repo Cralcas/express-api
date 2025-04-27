6200f7d1d4d99584ac7ffc68b24749ba1a3d43dc
Cralcas <carlosbergq@gmail.com>
2025-04-27 13:06:13 +0200

    added cors

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,11 @@
 import express from "express";
 import { router } from "./routes/apiRoutes";
+import cors from "cors";
 
 const app = express();
 const PORT = 8000;
+
+app.use(cors());
 
 app.use("/api", router);
 
