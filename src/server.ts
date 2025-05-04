8202b00790b7ebc1563681e7882816aa2930eb15
Cralcas <carlosbergq@gmail.com>
2025-05-04 11:24:11 +0200

    helmet + express-rate-limit

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,24 @@ import { router } from "./routes/apiRoutes";
 import cors from "cors";
 import { notFound } from "./middleware/not-found";
 import { error } from "./middleware/error";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 const PORT = 8000;
 
 app.use(cors());
+app.use(helmet());
 app.use(urlencoded({ extended: true }));
-
 app.use(json());
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+app.use(limiter);
 
 app.use("/api", router);
 
