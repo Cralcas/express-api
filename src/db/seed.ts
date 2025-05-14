0fd5875ed55831c7285cb2b9063a2d2ef602cd5e
Cralcas <carlosbergq@gmail.com>
2025-05-14 20:35:38 +0200

    mockdata fix

--- src/db/seed.ts
+++ src/db/seed.ts
@@ -1,11 +1,11 @@
 import "dotenv/config";
-import { db } from "./index";
-import { monarchs } from "./schema";
-import { monarchsData } from "../data/data";
+import { db } from "./index.js";
+import { monarchsTable } from "./schema.js";
+import { monarchsData } from "../data/data.js";
 
 async function seed() {
   try {
-    await db.insert(monarchs).values(monarchsData);
+    await db.insert(monarchsTable).values(monarchsData);
     console.log("Seeded monarch successfully!");
   } catch (error) {
     console.error("Failed to seed:", error);
